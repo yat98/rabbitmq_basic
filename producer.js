c1efb9789f15670feab90ee0002082fa219fcf0a
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-05 11:41:51 +0800

    add: producer

---
+++ producer.js
@@ -0,0 +1,18 @@
+import amqp from 'amqplib';
+
+const init = async () => {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+
+  const queue = 'dicoding';
+  const message = 'Selamat belajar message broker';
+
+  await channel.assertQueue(queue, {
+    durable: true,
+  });
+
+  await channel.sendToQueue(queue, Buffer.from(message));
+  console.log('Pesan berhasil terkirim');
+};
+
+init();
